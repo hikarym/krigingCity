0a70ef8f887bf75be6c5ee9e70fcab4932e2ae26
Miguel Angel Galarreta V <miguelg@ime.usp.br>
2017-11-11 05:21:20 -0200

    script to migrate data from complete database to schools

--- db/migration/ReadmeMigration.txt
+++ db/migration/ReadmeMigration.txt
@@ -13,3 +13,6 @@ replace ******
 
 execute the script using: 
     sudo -u postgres psql -d kriging_city_development -f dbfile6.sql
+
+
+insert into schools(identifier,longitude,latitude,code,nomeesc,created_at,updated_at) select id,cast(longitude as numeric),cast(latitude as numeric),codesctx,nomeesc,now(),now() from esc2013_rmsp_cem;
--- db/migration/insertCompleteData.sh
+++ db/migration/insertCompleteData.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
 sudo -u postgres psql -d kriging_city_development -f dbfile6.sql
+sudo -u postgres psql -d kriging_city_development -c 'insert into schools(identifier,longitude,latitude,code,nomeesc,created_at,updated_at) select id,cast(longitude as numeric),cast(latitude as numeric),codesctx,nomeesc,now(),now() from esc2013_rmsp_cem;'
